8bb6c5c52637464823fb96774876fcdf212864a2
anonynmous-exdash <joeizcooler@gmail.com>
2024-11-20 07:34:09 -0600

    Create switcher.js

---
+++ switcher.js
@@ -0,0 +1,28 @@
+// Made by anonynmous-exdash  for Maltion. Contact at joeizcooler@gmail.com
+(async()=>{
+console.log("%c Switching cloud ID", "font-size:40px;")
+try{
+
+if(!vm)var vm = document.getElementById("app")._reactRootContainer._internalRoot.current.child.pendingProps.store.getState().scratchGui.vm;
+
+function SwitchCloud(id="489159823"){
+  vm.runtime.ioDevices.cloud.provider.projectId = id
+  vm.runtime.ioDevices.cloud.provider.connection.close()
+}
+
+vm.runtime.ioDevices.cloud.setProvider = function(arg) {
+    console.log(arg)
+    if (!arg == null) {
+        vm.runtime.ioDevices.cloud.provider = arg;
+                SwitchCloud()
+    }
+}
+
+SwitchCloud()
+console.log("%c Succesfully switched cloud ID", "font-size:40px;")
+
+}catch(err){
+    console.error(err)
+    console.log("%c Something went wrong when switching cloud ID", "font-size:40px;")
+}
+})()
