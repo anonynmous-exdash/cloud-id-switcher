35a8d9c439741c009ac059bd8a0a1809cb14b0f5
Exdash <joeizcooler@gmail.com>
2024-11-20 16:13:03 -0600

    Update switcher.js

--- switcher.js
+++ switcher.js
@@ -1,28 +1,33 @@
 // Made by anonynmous-exdash  for Maltion. Contact at joeizcooler@gmail.com
-(async()=>{
-console.log("%c Switching cloud ID", "font-size:40px;")
-try{
+(async () => {
+  console.log("%c Switching cloud ID", "font-size:40px;");
+  try {
+    if (!vm)
+      var vm = document
+        .getElementById("app")
+        ._reactRootContainer._internalRoot.current.child.pendingProps.store.getState()
+        .scratchGui.vm;
 
-if(!vm)var vm = document.getElementById("app")._reactRootContainer._internalRoot.current.child.pendingProps.store.getState().scratchGui.vm;
-
-function SwitchCloud(id="489159823"){
-  vm.runtime.ioDevices.cloud.provider.projectId = id
-  vm.runtime.ioDevices.cloud.provider.connection.close()
-}
-
-vm.runtime.ioDevices.cloud.setProvider = function(arg) {
-    console.log(arg)
-    if (!arg == null) {
-        vm.runtime.ioDevices.cloud.provider = arg;
-                SwitchCloud()
+    function SwitchCloud(id = "489159823") {
+      vm.runtime.ioDevices.cloud.provider.projectId = id;
+      vm.runtime.ioDevices.cloud.provider.connection.close();
     }
-}
 
-SwitchCloud()
-console.log("%c Succesfully switched cloud ID", "font-size:40px;")
+    vm.runtime.ioDevices.cloud.setProvider = function (arg) {
+      console.log(arg);
+      if (!arg == null) {
+        vm.runtime.ioDevices.cloud.provider = arg;
+        SwitchCloud();
+      }
+    };
 
-}catch(err){
-    console.error(err)
-    console.log("%c Something went wrong when switching cloud ID", "font-size:40px;")
-}
-})()
+    SwitchCloud();
+    console.log("%c Succesfully switched cloud ID", "font-size:40px;");
+  } catch (err) {
+    console.error(err);
+    console.log(
+      "%c Something went wrong when switching cloud ID",
+      "font-size:40px;",
+    );
+  }
+})();
